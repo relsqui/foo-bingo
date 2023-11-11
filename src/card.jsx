b8a3db56f7124b1034fb7db951f368a98fcf8a76
Glitch (glitch-blank-react) <none>
2023-11-11 03:53:09 +0000

    🐺📍 Checkpoint ./src/styles.css:2714871/4333 ./src/card.jsx:2714871/1290

--- src/card.jsx
+++ src/card.jsx
@@ -1,14 +1,36 @@
 import * as React from "react";
 
+function Item({ content }) {
+  return (
+    <>
+      <div class="item">
+        <div class="item-content">
+          {content}
+        </div>
+      </div>
+    </>
+  );
+}
+
 export default function Card() {
-  const cardItems = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  const cardItems = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
 
   return (
     <>
       <div class="card">
-      {
-          cardItems.map(item => <div class="item">{item}</div>)
-        }
+        {cardItems.map((item) => (
+          <Item content={item} />
+        ))}
       </div>
     </>
   );
