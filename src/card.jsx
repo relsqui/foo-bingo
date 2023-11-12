b4e2dd270883ab2ffddb7f486d09abd647890bcb
Glitch (glitch-blank-react) <none>
2023-11-12 02:14:33 +0000

    🚵🌱 Checkpoint ./src/card.jsx:2714871/2196

--- src/card.jsx
+++ src/card.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import { items } from "./items.json";
 
 function Chip() {
@@ -12,11 +13,12 @@ function Chip() {
 }
 
 function Item({ content, hasChip }) {
+  const chip = hasChip ? <Chip /> : "";
   return (
     <>
       <button className="item">
         {content}
-        <Chip />
+        {chip}
       </button>
     </>
   );
@@ -25,17 +27,23 @@ function Item({ content, hasChip }) {
 function chooseItems() {
   // TODO: better shuffle
   const shuffled = [...items].sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, 16);
+  return shuffled.slice(0, 16).map((item) => {
+    return { item, hasChip: false };
+  });
 }
 
 export default function Card() {
-  const cardItems = chooseItems();
+  const [ cardItems, setCardItems ] = useState(() => chooseItems());
+  
+  function toggle(index) {
+    const newItems = {...cardItems};
+  }
 
   return (
     <>
       <div className="card">
-        {cardItems.map((item) => (
-          <Item content={item} key={item} />
+        {cardItems.map((item, index) => (
+          <Item content={item.item} key={index} />
         ))}
       </div>
     </>
